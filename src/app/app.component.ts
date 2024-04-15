58c3f5fb163cdf20294e8e5e59275efbbbf4b912
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-16 00:15:53 +0530

    separate modules admin and customer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'E-com_Angular_17';
+
+
+
+  ngOnInit(): void {
+
+
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:"",redirectTo:"/header",pathMatch:"full"},
+  {path:"header",component:HeaderComponent},
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)},
+  { path: 'customer', loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule)},
+
+];
