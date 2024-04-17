98e668d1eec5fcec41aa481ef2450a5d71abe8f7
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-17 23:49:53 +0530

    create admin service

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {AdminDashboardComponent} from "../admin/admin-dashboard/admin-dashboard.component";
-import {CustomerDashboardComponent} from "./customer-dashboard/customer-dashboard.component";
+import {CustomerDashboardComponent} from "./components/customer-dashboard/customer-dashboard.component";
 
 const routes:Routes =[
   {path:"dashboard",component:CustomerDashboardComponent}
