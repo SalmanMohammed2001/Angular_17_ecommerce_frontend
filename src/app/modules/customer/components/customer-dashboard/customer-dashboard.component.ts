cdcaa27c42a1f90c2c0e2be95f22c600c296ba5d
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-19 00:29:38 +0530

    customerDashboard Load All Product

--- src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AdminService} from "../../../admin/service/admin.service";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatDivider} from "@angular/material/divider";
+import {MatCard} from "@angular/material/card";
+import {RouterLink, RouterModule} from "@angular/router";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-customer-dashboard',
   standalone: true,
-  imports: [],
+    imports: [
+      MatDivider,
+      MatFormField,
+      MatButton,
+      MatCard,
+      RouterLink,
+      NgForOf,
+      ReactiveFormsModule,
+      FormsModule,
+      MatSnackBarModule,
+      RouterModule,
+      MatFormField,
+      MatInputModule,
+      MatButtonModule,
+      MatIcon,
+      MatFormField,
+      MatSelect,
+      MatOption,
+      MatButton,
+      MatInput,
+      NgForOf,
+      NgIf
+    ],
   templateUrl: './customer-dashboard.component.html',
   styleUrl: './customer-dashboard.component.scss'
 })
-export class CustomerDashboardComponent {
+export class CustomerDashboardComponent  implements OnInit{
 
+  products=[]
+  searchForm:FormGroup;
+
+
+  constructor(private adminService:AdminService,private  fb:FormBuilder,private  snackBar:MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.searchForm=this.fb.group({
+      title:['',[Validators.required]]
+    })
+    this.getAllProduct()
+  }
+
+  getAllProduct(){
+    this.products=[];
+    this.adminService.allProduct().subscribe((res)=>{
+      res.data.forEach((element: { processImg: string; byteImage: string; })=>{
+        element.processImg='data:image/jpeg;base64,'+element.byteImage
+        this.products.push(element)
+      });
+    });
+  }
+
+
+  onSubmit(){
+    const name=this.searchForm.get('title').value!
+    this.products=[];
+    this.adminService.allProductByName(name).subscribe((res)=>{
+      res.data.forEach((element: { processImg: string; byteImage: string; })=>{
+        element.processImg='data:image/jpeg;base64,'+element.byteImage
+        this.products.push(element)
+      });
+    })
+  }
 }
--- src/app/modules/customer/service/customer.service.ts
+++ src/app/modules/customer/service/customer.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {StorageService} from "../../../service/stroge/storage.service";
 
+const BASIC_URL='http://localhost:8080/'
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private  http:HttpClient) {
+
+  }
+
+
+  addCategory(category:any):Observable<any>{
+    return this.http.post(BASIC_URL+"api/v1/category/save",category,{
+      headers:this.createAuthorizationHeader(),
+    })
+  }
+
+
+
+
+
+  allProduct():Observable<any>{
+    return this.http.get(BASIC_URL+"api/v1/customer/list",{
+      headers:this.createAuthorizationHeader()
+    });
+  }
+  allProductByName(name:any):Observable<any>{
+    return this.http.get(BASIC_URL+`api/v1/customer/search/${name}`,{
+      headers:this.createAuthorizationHeader()
+    });
+  }
+
+  private createAuthorizationHeader() {
+    return new HttpHeaders().set(
+      "Authorization","Bearer "+StorageService.getToken()
+    )
+  }
 }
